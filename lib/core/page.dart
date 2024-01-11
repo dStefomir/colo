c99b83ddca4c3e59709b112f21988f801c235061
dimitrovs <stefomir.dimitrov@kahaura.com>
2024-01-11 21:13:58 +0200

    Orientation is now handled

--- lib/core/page.dart
+++ lib/core/page.dart
@@ -2,7 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:colo/model/account.dart';
 import 'package:colo/widgets/load.dart';
 import 'package:colo/widgets/page.dart';
+import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -29,51 +31,69 @@ class CorePage extends HookConsumerWidget {
   }) : super(key: key);
 
   /// Renders the default page widget
-  Widget _renderDefaultPage(BuildContext context, WidgetRef ref) => MainScaffold(
-    body: FutureBuilder<SharedPreferences>(
-        future: SharedPreferences.getInstance(),
-        builder: (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
-          if (snapshot.hasData) {
-            final sharedPrefs = snapshot.data;
-            /// Creates a stream build to listen for events in the fire store.
-            /// If there are any new events - it will reload the InitialPage with new data.
-            return StreamBuilder(
-                stream: FirebaseFirestore.instance.collection("users").doc(userId).snapshots(),
-                builder: (_, snapshot) {
-                  if (snapshot.hasData) {
-                    final account = Account.fromSnapshot(snapshot.data);
+  Widget _renderDefaultPage(BuildContext context, WidgetRef ref) {
+    final page = MainScaffold(
+      body: FutureBuilder<SharedPreferences>(
+          future: SharedPreferences.getInstance(),
+          builder: (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
+            if (snapshot.hasData) {
+              final sharedPrefs = snapshot.data;
+              /// Creates a stream build to listen for events in the fire store.
+              /// If there are any new events - it will reload the InitialPage with new data.
+              return StreamBuilder(
+                  stream: FirebaseFirestore.instance.collection("users").doc(userId).snapshots(),
+                  builder: (_, snapshot) {
+                    if (snapshot.hasData) {
+                      final account = Account.fromSnapshot(snapshot.data);
 
-                    return PopScope(
-                        onPopInvoked: (shouldPop) {
-                          if (onPopInvoked != null) {
-                            onPopInvoked!(shouldPop, ref);
-                          }
-                        },
-                        canPop: false,
-                        child: render(sharedPrefs!, account)
+                      return PopScope(
+                          onPopInvoked: (shouldPop) {
+                            if (onPopInvoked != null) {
+                              onPopInvoked!(shouldPop, ref);
+                            }
+                          },
+                          canPop: false,
+                          child: render(sharedPrefs!, account)
+                      );
+                    }
+
+                    return const BackgroundPage(
+                        child: Center(
+                            child: LoadingIndicator(color: Colors.purple)
+                        )
                     );
                   }
+              );
+            }
 
-                  return const BackgroundPage(
-                      child: Center(
-                          child: LoadingIndicator(color: Colors.purple)
-                      )
-                  );
-                }
-            );
-          }
-
-          return const BackgroundPage(
-              color: Colors.black,
-              child: Center(
-                child: LoadingIndicator(
-                  color: Colors.purple
+            return const BackgroundPage(
+                color: Colors.black,
+                child: Center(
+                    child: LoadingIndicator(
+                        color: Colors.purple
+                    )
                 )
-              )
-          );
-        }),
-    resizeToAvoidBottomInset: resizeToAvoidBottomInset,
-  );
+            );
+          }),
+      resizeToAvoidBottomInset: resizeToAvoidBottomInset,
+    );
+
+    /// If game is started then allow only portrait orientation.
+    /// If its the initial screen then allow all orientations.
+    return pageName == 'Game'
+        ? FutureBuilder(
+        future: Flame.device.setOrientation(DeviceOrientation.portraitUp),
+        builder: (_, __) => page)
+        : FutureBuilder(
+        future: Flame.device.setOrientations(
+            [
+              DeviceOrientation.portraitUp,
+              DeviceOrientation.portraitDown,
+              DeviceOrientation.landscapeLeft,
+              DeviceOrientation.landscapeRight
+            ]
+        ), builder: (_, __) => page);
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) => _renderDefaultPage(context, ref);
--- lib/module/game/component/manager/bullet.dart
+++ lib/module/game/component/manager/bullet.dart
@@ -7,13 +7,6 @@ import 'package:flutter/material.dart';
 /// Manger for controlling the bullet rules
 class BulletManager extends Component {
 
-  @override
-  Future<void> onGameResize(Vector2 size) async {
-    super.onGameResize(size);
-    final ColoGamePage game = parent!.parent as ColoGamePage;
-    game.removeAll(game.children.whereType<Bullet>());
-  }
-
   /// Gets a riv file bullet color based on the game level
   String getBulletRivAssetBasedOnColor({required Color color}) => bulletColors.entries.firstWhere((element) => element.value == color).key;
   /// Gets a dy limit for the bullet based on the game level
